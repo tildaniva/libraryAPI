d4eea92210b01ff1ed37da91d621e4cc23ecd700
tildanivagmailcom <tilda.niva@gmail.com>
2026-04-13 15:27:22 +0200

    Fixed Bookrequest

--- src/main/java/com/example/LibraryAPI/dto/BookRequest.java
+++ src/main/java/com/example/LibraryAPI/dto/BookRequest.java
@@ -1,10 +1,13 @@
 package com.example.LibraryAPI.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Book request")
 public class BookRequest {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
     private String isbn;
     private int publicationYear;
@@ -23,5 +26,21 @@ public class BookRequest {
 
     public String getAuthor() {
         return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
     }
 }
