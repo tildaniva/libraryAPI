32d00781186c1fa10861563f2ea48bd266218620
tildanivagmailcom <tilda.niva@gmail.com>
2026-04-15 11:48:26 +0200

    Created Loan

---
+++ src/main/java/com/example/LibraryAPI/model/Loan.java
@@ -0,0 +1,48 @@
+package com.example.LibraryAPI.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "loans", uniqueConstraints = @UniqueConstraint(columnNames = "book_id"))
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "book_id", unique = true)
+    private Book book;
+
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+
+    public Loan(){}
+    public Loan(Book book){
+        this.book = book;
+        this.loanDate = LocalDate.now();
+        this.returnDate = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+}
+
